ad8093e7b961e255e4359041b134afcdc76f6707
Lucas de Capra <lucas.capra@mobilesaude.com.br>
2023-05-28 10:37:07 -0300

    adiciona https nas rotas

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
+import fs from "fs";
 import express, { Request, Response, Application } from "express";
+import https from "https";
 import dotenv from 'dotenv'
 import userRouter from "./routes/Users/index.js";
 import {mongoHelper} from "./helpers/MongoDb/index.js";
@@ -12,14 +14,17 @@ const createServer = () : Application => {
     app.use(cors())
     app.use(express.json()) //middleware do express que faz o parse da request
     const port = process.env.PORT || 3001
-    app.get("/health", (req: Request, res: Response) => {
+    app.get("/v1/health", (req: Request, res: Response) => {
         res.send("UP");
     });
     app.use('/v1', api)
     // app.use(userRouter)
     // app.use(loginRouter)
-    app.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
+    https.createServer({
+        key:fs.readFileSync('key.pem'),
+        cert:fs.readFileSync('cert.pem')
+    }, app).listen(port, () => {
+        console.log(`Server running at https://localhost:${port}/v1`);
     });
 
     return app
