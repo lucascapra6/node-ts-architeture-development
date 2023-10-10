d5533666e0f946cf38750c00efee3406b592f2ec
Lucas de Capra <lucas.capra@mobilesaude.com.br>
2023-10-10 17:29:37 -0300

    adiciona docker file

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,24 @@
-
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: [
-        "/node_modules/",
-        "/dist/"
-    ],
+    preset: "ts-jest/presets/default-esm",
+    testEnvironment: "node",
+    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
     roots: [
         //"<rootDir>/src"
-        "<rootDir>"
+        "<rootDir>",
     ],
+    globals: {
+        "ts-jest": {
+            useESM: true,
+        },
+    },
     testMatch: [
         "**/__tests__/**/*.+(ts|tsx|js)",
-        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+        "**/?(*.)+(spec|test).+(ts|tsx|js)",
     ],
     transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest"
+        "^.+\\.(ts|tsx)$": "ts-jest",
     },
     moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': "$1",
-    }
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
 };
